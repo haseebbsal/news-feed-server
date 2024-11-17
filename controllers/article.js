971c6a921565078c354edb6a234b4765656fa97f
Haseeb <haseeb.salman1234@outlook.com>
2024-11-17 15:57:01 +0500

    new version

--- controllers/article.js
+++ controllers/article.js
@@ -9,6 +9,7 @@ const OpenAi = require('openai')
 const openai = new OpenAi({ apiKey: process.env.OPENAI_API_KEY })
 
 
+
 const publishArticle = async (req, res) => {
     const validateResult = validationResult(req)
     if (!validateResult.isEmpty()) {

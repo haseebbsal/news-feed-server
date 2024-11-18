3673ac505e616b611ec8c80b167e15915447c914
Haseeb <haseeb.salman1234@outlook.com>
2024-11-19 04:19:41 +0500

    new version

--- utils/index.js
+++ utils/index.js
@@ -134,9 +134,9 @@ const dissbotFetchArticle = async (url) => {
             } else {
                 // console.log('Content:', article.content);
     
-                const text = extractTextFromHTML(article.content);
+                // const text = extractTextFromHTML(article.content);
                 // console.log('Extracted Text:', text);
-                resolve({html:article.content,link:url,text,title:article.title})
+                resolve({html:article.content,link:url,text:article.textBody,title:article.title})
             }
         });
     })

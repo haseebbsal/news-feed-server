1e09a222e2a06180063f56faa979ad8c881c6781
Haseeb <haseeb.salman1234@outlook.com>
2024-11-21 03:20:50 +0500

    new version

--- test.js
+++
@@ -1,6 +0,0 @@
-const extractt=require('article-parser')
-
-const input = 'https://edition.cnn.com/business/live-news/fox-news-dominion-trial-04-18-23/index.html'
-extractt.extract(input)
-  .then(article => console.log(article))
-  .catch(err => console.error(err))

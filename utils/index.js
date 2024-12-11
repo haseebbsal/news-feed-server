caef00455817c03282b1c75aea287e313c5ea75f
Haseeb <haseeb.salman1234@outlook.com>
2024-12-12 03:30:06 +0500

    push

--- utils/index.js
+++ utils/index.js
@@ -158,7 +158,7 @@ const dissbotFetchArticle = async (url) => {
     let content
     let title
     try {
-        const browser = await puppeteer.launch({headless:false,executablePath: '/usr/bin/chromium-browser',ignoreDefaultArgs: ['--disable-extensions']});
+        const browser = await puppeteer.launch({headless:false,executablePath: '/usr/bin/chromium-browser',ignoreDefaultArgs: ['--disable-extensions'],args:['--no-sandbox','--disable-setuid-sandbox']});
         const page = await browser.newPage();
 
         // Navigate the page to a URL

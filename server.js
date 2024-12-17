3d5ac48d90eabbd55956f0d9af5ede0e3bd3c4b9
Haseeb <haseeb.salman1234@outlook.com>
2024-12-18 02:35:18 +0500

    push

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
 }))
 app.use(helmet())
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json({limit:"1tb"}))
 app.use('/api', routeRouter)
 
 async function connect() {
